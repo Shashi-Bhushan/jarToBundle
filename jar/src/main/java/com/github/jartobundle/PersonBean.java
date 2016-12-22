c39db55d4cda75191fe807da72ecfa909526ba53
Shashi Bhushan <shabhushan@deloitte.com>
2016-12-22 15:16:16 +0530

    Bean Added

---
+++ jar/src/main/java/com/github/jartobundle/PersonBean.java
@@ -0,0 +1,21 @@
+package com.github.jartobundle;
+
+/**
+ * Created by Shashi on 12/22/2016.
+ */
+public class PersonBean {
+
+    private String name;
+
+    public PersonBean(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
