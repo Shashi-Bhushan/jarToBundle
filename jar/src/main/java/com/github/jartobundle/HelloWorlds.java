4a4ba51d7d8c7e902ba46f38419a60fd86eb532d
Shashi Bhushan <shabhushan@deloitte.com>
2016-12-22 15:12:10 +0530

    Changes to Jar

---
+++ jar/src/main/java/com/github/jartobundle/HelloWorlds.java
@@ -0,0 +1,10 @@
+package com.github.jartobundle;
+
+/**
+ * Created by shabhushan on 12/22/2016.
+ */
+public class HelloWorlds {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
